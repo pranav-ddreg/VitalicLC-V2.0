b9505d244a5bb51dfced12b5180429d5d9d4a933
pranav-ddreg <pranav.c@ddreg.in>
2025-10-14 12:00:59 +0530

    Husky Init test

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
+            Get started by Husky{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               app/page.tsx
             </code>
